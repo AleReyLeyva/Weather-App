ce0dc424a1d08a70f554a832c26909621320c6c2
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-21 05:58:18 +0200

    Add tailwind purge option to views component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './views/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     colors: {
-      primary: "#100E1D",
-      secondary: "#1E213A",
+      primary: '#100E1D',
+      secondary: '#1E213A',
       gray: {
-        300: "#E7E7EB",
-        400: "#A09FB1",
-        500: "#6E707A",
-        600: "#88869D, ",
+        300: '#E7E7EB',
+        400: '#A09FB1',
+        500: '#6E707A',
+        600: '#88869D, ',
       },
-      white: "#fff",
-      transparent: "transparent",
-      blue: "#3C47E9",
-      yellow: "#FFEC65",
+      white: '#fff',
+      transparent: 'transparent',
+      blue: '#3C47E9',
+      yellow: '#FFEC65',
     },
   },
   variants: {
