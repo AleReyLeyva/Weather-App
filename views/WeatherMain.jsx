07bc799085d55d05cfee578cde455c07d9836eff
AleReyLeyva <65025739+AleReyLeyva@users.noreply.github.com>
2021-08-23 12:11:46 +0200

    Loading state and conditional render refactor

--- views/WeatherMain.jsx
+++ views/WeatherMain.jsx
@@ -10,33 +10,45 @@ export default function WeatherMain() {
   const { weather } = useWeather();
   const { loading, weatherInfo, location } = weather;
 
-  return (
-    <div className="main-container">
-      {!loading && location ? (
-        <div className="px-16 pt-10 pb-6 flex flex-col min-h-screen w-full">
-          <div className="day-cards-grid">
-            {weatherInfo.map(
-              (w, key) =>
-                key > 0 && (
-                  <DayWeather weather={w} key={key} tomorrow={key === 1} />
-                ),
-            )}
-          </div>
-          <div className="w-full mt-8">
-            <h1 className="w-full text-left">Today&apos;s Highlights</h1>
-            <div className="highlight-cards-grid">
-              <WindCard />
-              <HumidityCard />
-              <VisibilityCard />
-              <AirCard />
-            </div>
-          </div>
-        </div>
-      ) : (
+  if (loading)
+    return (
+      <div className="main-container">
+        <h1 className="w-1/2 mx-auto h-screen flex justify-center items-center text-center">
+          Loading weather...
+        </h1>
+      </div>
+    );
+
+  if (!location)
+    return (
+      <div className="main-container">
         <h1 className="w-1/2 mx-auto h-screen flex justify-center items-center text-center">
           No information about weather
         </h1>
-      )}
+      </div>
+    );
+
+  return (
+    <div className="main-container">
+      <div className="px-16 pt-10 pb-6 flex flex-col min-h-screen w-full">
+        <div className="day-cards-grid">
+          {weatherInfo.map(
+            (w, key) =>
+              key > 0 && (
+                <DayWeather weather={w} key={key} tomorrow={key === 1} />
+              ),
+          )}
+        </div>
+        <div className="w-full mt-8">
+          <h1 className="w-full text-left">Today&apos;s Highlights</h1>
+          <div className="highlight-cards-grid">
+            <WindCard />
+            <HumidityCard />
+            <VisibilityCard />
+            <AirCard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
